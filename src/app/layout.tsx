4587896d5c0304cad4b8ad9e7fa2b4860faaceb8
Ldn26 <y_laidani@estin.dz>
2025-12-12 02:57:30 +0100

    build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import { WixClientContextProvider } from "@/context/wixContext";
+import { WixClientContextProvider } from "@/context/WixContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
--- src/hooks/useWixClient.tsx
+++ src/hooks/useWixClient.tsx
@@ -1,6 +1,6 @@
  "use client"
 import {useContext} from "react" 
-import { WixClientContext } from "@/context/wixContext";
+import { WixClientContext } from "@/context/WixContext";
 export const  useWixClient =()=>{
     return useContext(WixClientContext)
    } 
